0c014dcf6e1ebc4ea8a3c8440a1e9067fabad1b2
mchlltt <me@northwestern.edu>
2017-01-28 12:43:18 -0600

    Initial commit

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// NPM dependency.
+var mysql = require('mysql');
+
+// Connect to JAWS_DB if on Heroku, or a local MySQL instance if not.
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '5555',
+        database: 'burgers_db'
+    });
+}
+
+// Make connection.
+connection.connect();
+
+module.exports = connection;
