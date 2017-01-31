0a67457caf866bede54b6b0deea5227fef779baf
mchlltt <me@northwestern.edu>
2017-01-30 20:44:03 -0600

    Add seed data

--- models/burger.js
+++ models/burger.js
@@ -28,15 +28,12 @@ module.exports = function(sequelize, DataTypes) {
             defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
         }
     });
-//
-// Burger.bulkCreate([
-//     {burger_name: 'Hamburger'},
-//     {burger_name: 'Cheeseburger'},
-//     {burger_name: 'Veggie Burger'}
-// ],
-//     {
-//         updateOnDuplicate
-//     });
+
+    Burger.bulkCreate([
+        {id: 1, burger_name: 'Hamburger'},
+        {id: 2, burger_name: 'Cheeseburger'},
+        {id: 3, burger_name: 'Veggie Burger'}
+    ], {ignoreDuplicates: true});
 
     Burger.sync();
 
